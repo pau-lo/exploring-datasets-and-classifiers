8ee8803e114b5ca188264d0b6cd9bfa9780de4fd
Paulo Lopez <pau_lo_lop_ez@protonmail.com>
2020-11-10 20:57:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,26 +8,35 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+    plot_confusion_matrix,
+
+)
 from sklearn.decomposition import PCA
 
 
 def main():
-    st.title("Streamlit Example")
+    st.title("A webapp that explores different datasets and classifiers.")
 
     st.write("""
-    # Explore different classifiers and different datasets
-    Which one works best?
+    ### Explore the Breast Cancer, Iris, Wine and Digits datasets using Scikit Learn.
     """)
 
+    st.sidebar.write("### Choose the best classifier and parameter")
     data_name = st.sidebar.selectbox("Select Dataset:",
-                                     ("Breast Cancer", "Iris", "Wine",
-                                      "Digits")
+                                     ("Breast Cancer", "Iris",
+                                      "Wine", "Digits")
                                      )
     classifier_name = st.sidebar.selectbox("Select Classifier:",
-                                           ("Logistic Regression", "KNN",
-                                            "SVM", "Random Forest")
+                                           ("Logistic Regression",
+                                            "K-Nearest Neighbors (KNN)",
+                                            "Support Vector Machine (SVM)",
+                                            "Random Forest")
                                            )
 
     @st.cache(persist=True)
@@ -40,28 +49,34 @@ def main():
             data = datasets.load_wine()
         else:
             data = datasets.load_digits()
-        # split data
+
+        # split features and target
         X = data.data
         y = data.target
-        return X, y
+        class_names = data.target_names
+        return X, y, class_names
 
-    X, y = load_data()
-    st.write("Load data: ", data_name)
-    st.write("Shape of dataset", X.shape)
+    X, y, class_names = load_data()
+
+    st.write("Current data: ", data_name)
+    st.write("Shape of", data_name, "dataset ", X.shape)
+    st.write("Class labels: ",  class_names)
+
+    st.sidebar.write("Adjust parameters: ")
 
     def add_parameter(classifier_name):
         params = dict()
         if classifier_name == "Logistic Regression":
             C = st.sidebar.slider("C", 0.01, 10.0)
             params["C"] = C
-        elif classifier_name == "KNN":
+        elif classifier_name == "K-Nearest Neighbors (KNN)":
             K = st.sidebar.slider("K", 1, 15)
             params["K"] = K
-        elif classifier_name == "SVM":
+        elif classifier_name == "Support Vector Machine (SVM)":
             C = st.sidebar.slider("C", 0.01, 10.0)
             params["C"] = C
         else:
-            # classifier_name == "Random Forest":
+            # load random forest
             max_depth = st.sidebar.slider("max_depth", 2, 15)
             n_estimators = st.sidebar.slider("n_estimators", 1, 100)
             params["max_depth"] = max_depth
@@ -73,9 +88,9 @@ def main():
     def get_classifier(clf_name, params):
         if classifier_name == "Logistic Regression":
             clf = LogisticRegression(C=params["C"], solver='liblinear')
-        elif classifier_name == "KNN":
+        elif classifier_name == "K-Nearest Neighbors (KNN)":
             clf = KNeighborsClassifier(n_neighbors=params["K"])
-        elif classifier_name == "SVM":
+        elif classifier_name == "Support Vector Machine (SVM)":
             clf = SVC(C=params["C"])
         else:
             clf = RandomForestClassifier(
@@ -86,41 +101,70 @@ def main():
 
     clf = get_classifier(classifier_name, params)
 
+    # split data for training and testing
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=23)
 
-    scaler = MinMaxScaler()
+    # standardized features
+    scaler = StandardScaler()
     scaler.fit_transform(X_train)
 
+    # fit model
     clf.fit(X_train, y_train)
+
+    # predict
     y_pred = clf.predict(X_test)
 
-    accuracy = accuracy_score(y_test, y_pred)
+    # calculate metrics
+    accuracy = accuracy_score(y_test, y_pred).round(3)
+    precision = precision_score(y_test, y_pred,
+                                average='weighted', zero_division=0).round(3)
+    recall = recall_score(y_test, y_pred,
+                          average='weighted').round(3)
 
-    accuracy_results = {
-        "model_name": classifier_name,
-        "model_accuracy": accuracy,
+    model_results = {
+        "Model Name": classifier_name,
+        "Accuracy": accuracy,
+        "Precision": precision,
+        "Recall Score": recall
+        # "ROC AUC Score": area_under_curve
     }
 
     st.write("Model Results: ")
-    st.json(accuracy_results)
+    st.json(model_results)
+
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+
+    # confusiont matrix plot
+    def confusion_matrix_plot():
+        st.subheader("Confusion Matrix")
+        plt.figure(1, figsize=(9, 7))
+        plot_confusion_matrix(clf, X_test, y_test,
+                              display_labels=class_names)
+        st.pyplot()
+        
+    # instantiate plot
+    confusion_matrix_plot()
+
+    # pca plot
 
     pca = PCA(n_components=2, svd_solver='full')
     X_projected = pca.fit_transform(X)
     x1 = X_projected[:, 0]
     x2 = X_projected[:, 1]
 
-    st.write(f"Principal Component Analysis for {data_name}")
+    # st.write(f"Principal Component Analysis for {data_name}")
 
     def pca_plot():
-        fig = plt.figure(1, figsize=(8, 6))
-        # plt.clf()
-        # ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+        st.subheader("PCA Scatter Plot")
+        fig = plt.figure(1, figsize=(9, 7))
         plt.scatter(x1, x2, c=y, alpha=0.8, cmap='Paired')
         plt.xlabel("Principal Component 1")
         plt.ylabel("Principal Component 2")
         plt.colorbar()
         st.pyplot(fig)
+
+    # instantiate plot
     pca_plot()
 
 
